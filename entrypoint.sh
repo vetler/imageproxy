918220624a4599e2bf9e4c0b17cc3193541c5d8d
Vetle Leinonen-Roeim <vetle@reprice.no>
2025-04-08 20:45:05 +0200

    fix: update entrypoint script to use variable for ADDR in base command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Ensure ADDR is set to 0.0.0.0:8080
+export ADDR="0.0.0.0:8080"
+
 # Base command
-CMD="/app/imageproxy -addr 0.0.0.0:8080"
+CMD="/app/imageproxy -addr $ADDR"
 
 # Add -signatureKey if SIGNATURE_KEY is defined
 if [ -n "$SIGNATURE_KEY" ]; then
